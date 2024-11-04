3e4061687b6617d43950932ee6c2cf7e4636cb2c
= <=>
2024-11-03 18:55:36 -0800

    Login form toggle

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Login.css'
+import { useState } from 'react';
 
 const onSubmitHandler = (event) => {
     event.preventDefault();
@@ -11,12 +12,14 @@ const onSubmitHandler = (event) => {
   }
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState("Sign up");
+
   return (
     <div className="login">
         <img src="" alt="image" className="logo" />
         <form action="" className="login-form">
-            <h2>Sign Up</h2>
-            <input type="text" placeholder="username" className="form-input" required/>
+            <h2>{currentState}</h2>
+            {currentState==="Sign up" ? <input type="text" placeholder="username" className="form-input" required/> : null}
             <input type="email" placeholder="email address" className="form-input" required/>
             <input type="password" placeholder="password" className="form-input" required/>
             <button type="submit">Sign Up</button>
@@ -25,7 +28,13 @@ const Login = () => {
                 <p>Agree to the terms of use & privacy policy</p>
             </div>
             <div className="login-forgot">
-                <p className="login-toggle">Already have an account <span> click here </span> </p>
+              {
+                currentState==="Sign up" 
+                  ?
+                  <p className="login-toggle">Already have an account <span onClick={()=>setCurrentState("Login")}> Login here </span> </p>
+                  :
+                  <p className="login-toggle">Create an account <span onClick={()=>setCurrentState("Sign up")}> Click here </span> </p>
+              }
             </div>
         </form>
     </div>
