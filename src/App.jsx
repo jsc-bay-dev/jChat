2f35943fa5b8cc8bbbaa80f58f5752710a715507
= <=>
2024-11-03 18:00:28 -0800

    Login styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login/Login'
 import Chat from './pages/Chat/Chat'
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Login.css'
 
+const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (currState === "Sign up") {
+      signup(userName, email, password);
+    } else {
+      login(email, password);
+    }
+  }
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+        <img src="" alt="" className="logo" />
+        <form action="" className="login-form">
+            <h2>Sign Up</h2>
+            <input type="text" placeholder="username" className="form-input" />
+            <input type="email" placeholder="email address" className="form-input" />
+            <input type="password" placeholder="password" className="form-input" />
+            <button type="submit">Sign Up</button>
+            <div className="login-term">
+                <input type="checkbox" />
+                <p>Agree to the terms of use & privacy policy</p>
+            </div>
+            <div className="login-forgot">
+                <p className="login-toggle">Already have an account <span> click here </span> </p>
+            </div>
+        </form>
+    </div>
   )
 }
 
